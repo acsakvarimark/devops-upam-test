9ff3788e23bbea3de0bd3122c00948198792226a
WaalooR <markcsakvari23@gmail.com>
2025-03-04 22:16:09 +0100

    Update docker_setup.sh

--- docker_setup.sh
+++ docker_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Installing docker"
-apt-get update && sudo apt-get install -y docker.io
-systemctl enable --now docker
+sudo apt-get update && sudo apt-get install -y docker.io
+sudo systemctl enable --now docker
 
 echo "Pulling ansible"
-docker pull ansible/ansible:latest
-docker run --rm -i -t ansible/ansible:latest ansible --version
+sudo docker pull ansible/ansible:latest
+sudo docker run --rm -i -t ansible/ansible:latest ansible --version
