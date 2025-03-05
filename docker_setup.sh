3c97b3caf86b96398337d2e60d0f47f54c706076
acsakvarimark <markcsakvari23@gmail.com>
2025-03-05 11:11:35 +0100

    Update docker_setup.sh

--- docker_setup.sh
+++ docker_setup.sh
@@ -4,6 +4,9 @@ echo "Installing docker"
 sudo apt-get install -y docker.io
 sudo systemctl enable --now docker
 
-echo "Pulling ansible"
-sudo docker pull ansible/ansible:ubuntu1604
-sudo docker run --rm --privileged -t ansible/ansible:ubuntu1604 bash -c "ansible --version"
+
+echo "Installing ansible"
+sudo apt-get update
+sudo apt-get install -y ansible
+ansible --version
+
